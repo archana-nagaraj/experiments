2187eeb00e8851a11a54f036fd1d2e32366f7185
Archana Nagaraja <archananagaraja@Archanas-MacBook-Air.local>
2019-07-17 17:26:31 -0700

    exploring __name__ and __main__

---
+++ codeSnippets/firstModule.py
@@ -0,0 +1,7 @@
+print("Running firstmodule directly: __name__ =  "+ __name__)
+
+def my_function():
+    print("I am inside my function")
+
+if __name__ == '__main__':
+    my_function()
---
+++ codeSnippets/secondModule.py
@@ -0,0 +1,3 @@
+import firstModule
+
+print("Running second module directly with importing firstmodule: __name__ =" +__name__)
